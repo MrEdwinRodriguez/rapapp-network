63ce9fb7d919139d86dc010d12ae74a5558725eb
Edwin <mredwinrodriguez@gmail.com>
2021-03-29 12:52:27 -0400

    connect to db and setup api routes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
+connectDB();
+
 app.get('/', (req, res) => res.send("Server is running"));
+
+app.use('/api/user', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/recordings', require('./routes/api/recordings'));
 
 const PORT = process.env.PORT || 5010;
 
