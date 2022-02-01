72a476bc7cc84b10cd08ac52f3ba0ec08f2e2d74
Edwin <mredwinrodriguez@gmail.com>
2022-02-01 08:43:15 -0500

    setup navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 
 const App = () => (
     <Router>
       <Fragment>
+        <Navbar />
         <Route exact path='/' component={Landing} />
       </Fragment>
     </Router>
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+import { Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+
+export const Navbar = () => {
+    const guestLinks = (
+        <ul>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="login">Login</Link></li>
+        </ul>
+      );
+    return (
+        <nav className="navbar bg-dark">
+        <Fragment>guestLinks</Fragment>
+      </nav>
+    )
+}
+
+export default Navbar
